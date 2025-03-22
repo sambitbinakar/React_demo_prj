014147fda4ecc9be84fbc698fd7eaba8176c9b4c
Sambit <sambitbinakar@gmail.com>
2025-03-22 14:49:12 +0530

    bug3

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  esbuild: {
-    target: 'esnext',
-    platform: 'linux',
-  },
+  
   plugins: [react(),
     tailwindcss()],
   base: process.env.VITE_BASE_PATH || "/React_demo_prj",
